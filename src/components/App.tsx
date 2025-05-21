5fe750a0018c7f7a29e1191cbeb5aa874b636596
Luiz Antonio <louis_work80@outlook.com>
2025-05-21 19:20:44 -0300

    Add React

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <h1>Welcome to Electron URLs</h1>
+      <p>This is a desktop app to test API endpoints.</p>
+      <ul>
+        <li>Create and manage API requests</li>
+        <li>View responses in real time</li>
+        <li>Save your favorite endpoints</li>
+      </ul>
+    </main>
+  )
+}
