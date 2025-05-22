a615e7bab81278802f29024e32ab7536bc13fe16
Luiz Antonio <louis_work80@outlook.com>
2025-05-21 22:42:54 -0300

    Create the url input section

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,14 @@
+import Bar from './Bar/Bar'
 import './App.css'
 
 export default function App() {
   return (
     <main>
-      <h1>Welcome to Electron URLs</h1>
-      <p>This is a desktop app to test API endpoints.</p>
-      <ul>
-        <li>Create and manage API requests</li>
-        <li>View responses in real time</li>
-        <li>Save your favorite endpoints</li>
-      </ul>
+      <Bar
+        onSend={() => {
+          console.log('request sent')
+        }}
+      />
     </main>
   )
 }
---
+++ src/components/Bar/Bar.tsx
@@ -0,0 +1,54 @@
+import { RiArrowDropDownLine } from 'react-icons/ri'
+import { useState } from 'react'
+import './Bar.css'
+
+enum RequestType {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
+
+interface Props {
+  onSend: () => void
+}
+
+export default function Bar({ onSend }: Props) {
+  const [isDropdownActive, setIsDropdownActive] = useState<boolean>(false)
+  const [selectedType, setSelectedType] = useState<RequestType>(RequestType.GET)
+
+  function handleRequestTypeClick(type: RequestType) {
+    setIsDropdownActive(false)
+    setSelectedType(type)
+  }
+
+  return (
+    <div className='bar'>
+      <button
+        className='request-type'
+        onClick={() => {
+          setIsDropdownActive(true)
+        }}
+      >
+        <span>{selectedType.toString()}</span>
+        <RiArrowDropDownLine size={24} />
+      </button>
+      <div className='divider'></div>
+      <input type='text' placeholder='url...' />
+      <button className='send' onClick={onSend}>
+        Send
+      </button>
+
+      {isDropdownActive && (
+        <ul className='request-options'>
+          <li onClick={() => handleRequestTypeClick(RequestType.GET)}>GET</li>
+          <li onClick={() => handleRequestTypeClick(RequestType.POST)}>POST</li>
+          <li onClick={() => handleRequestTypeClick(RequestType.PUT)}>PUT</li>
+          <li onClick={() => handleRequestTypeClick(RequestType.DELETE)}>
+            DELETE
+          </li>
+        </ul>
+      )}
+    </div>
+  )
+}
